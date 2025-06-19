fa5a58151ec5439dd7e62c9fb8e7b94a4b70c516
Shubham Kumar <shukumar@gridynamics.com>
2025-06-19 15:42:18 +0530

    second commit

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
-templates = Jinja2Templates(directory="app/templates")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/page1", response_class=HTMLResponse)
